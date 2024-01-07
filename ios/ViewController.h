be0fc055e75f6ea8cd98675f2e658fecd823f6f9
Федор <kfv@iMac-Fedor.local>
2024-01-07 15:41:00 +0300

    fix

--- ios/ViewController.h
+++ ios/ViewController.h
@@ -1,9 +1,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-@property (nonatomic) CGSize screenSize;
-@property (nonatomic) CFTimeInterval resizeViewAnimationStartTime;
-@property (nonatomic) CFTimeInterval resizeViewAnimationDuration;
-@property (nonatomic) CADisplayLink *displayLink;
 
 @end
